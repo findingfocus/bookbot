ae30701ccf58377731c0839dbd88ee7bcd422a39
Paul Thompson <pthompson27@cnm.edu>
2024-06-06 21:44:51 -0600

    added book report section

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     word_count = get_word_count(text)
-    print(f"Word count: {word_count}")
+    chars_dict = get_character_count(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +23,25 @@ def get_book_text(path):
 def get_word_count(text):
     return len(text.split())
 
-if __name__ == "__main__":
-    main()
+def get_character_count(text):
+    characters = { 'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0 }
+    lower_text = text.lower()
+    for character in lower_text:
+        if character in characters:
+            characters[character] += 1
+    return characters
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def sort_on(dict):
+    return dict["num"]
+
+main()
+
+
